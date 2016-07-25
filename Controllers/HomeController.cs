e67a322184517aa929d1347f6fd638c6290bf9d3
Viktor Kotsiuban <viktor.kotsiuban@gmail.com>
2016-07-25 09:46:54 +0300

    Add register new player

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -2,12 +2,20 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using HabraQuest.Model;
 using Microsoft.AspNetCore.Mvc;
 
 namespace HabraQuest.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly DataContext dataContext;
+
+        public HomeController(DataContext db)
+        {
+            dataContext = db;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -16,6 +24,19 @@ namespace HabraQuest.Controllers
         public IActionResult Error()
         {
             return View();
+        }
+
+        public string RegisterNewPlayer()
+        {
+            Player player = new Player
+            {
+                Token = Guid.NewGuid()
+            };
+
+            dataContext.Add(player);
+            dataContext.SaveChanges();
+
+            return player.Token.ToString();
         }
     }
 }
